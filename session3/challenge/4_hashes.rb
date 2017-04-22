ce61dd54c2cf73ffa751fdefe6df55726972af48
kye.bracey@gmail.com <kye.bracey@gmail.com>
2017-04-22 14:03:31 +0000

    Solved 3:4

--- session3/challenge/4_hashes.rb
+++ session3/challenge/4_hashes.rb
@@ -5,3 +5,10 @@
 # There will be no punctuation in the strings.
 #
 # first_pos "The dog and the cat and the cow" # => {"The" => 0, "dog" => 1, "and" => 2, "the" => 3, "cat" => 4, "cow" => 7}
+
+def first_pos(string)
+    to_return = Hash.new
+    arr = string.split
+    arr.each {|i| to_return[i] = arr.index(i)}
+    to_return
+end
