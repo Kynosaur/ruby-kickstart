bb3d33412e8644b2bde39946b6e937b3d65dae20
kye.bracey@gmail.com <kye.bracey@gmail.com>
2017-04-23 11:04:55 +0000

    Solved 3:11

--- session3/challenge/11_blocks_or_procs.rb
+++ session3/challenge/11_blocks_or_procs.rb
@@ -30,5 +30,10 @@
 # end
 
 
-def array_init
+def array_init (indexes=5, &block)
+    if block_given?
+        Array.new(indexes, &block)
+    else
+        Array.new(indexes){|i| i = (i*100).to_s}
+    end
 end
